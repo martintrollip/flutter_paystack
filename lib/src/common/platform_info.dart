c64e60575b07866d538bc8cefab433c16825db13
Martin Trollip <13981100+martintrollip@users.noreply.github.com>
2024-06-14 12:54:50 +0200

    feat: Web - Initial web setup

--- lib/src/common/platform_info.dart
+++ lib/src/common/platform_info.dart
@@ -1,6 +1,9 @@
 import 'dart:io';
+import 'dart:html' as html;
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:uuid/uuid.dart';
 
 /// Holds data that's different on Android and iOS
 class PlatformInfo {
@@ -10,17 +13,41 @@ class PlatformInfo {
 
   static Future<PlatformInfo> fromMethodChannel(MethodChannel channel) async {
     // TODO: Update for every new versions.
-    //  And there should a better way to fucking do this
     final pluginVersion = "2.0.0";
 
-    final platform = Platform.operatingSystem;
+    final platform = kIsWeb ? 'web' : Platform.operatingSystem;
     String userAgent = "${platform}_Paystack_$pluginVersion";
-    String deviceId = await channel.invokeMethod('getDeviceId') ?? "";
+    String deviceId = await getDeviceId(channel);
     return PlatformInfo._(
       userAgent: userAgent,
       paystackBuild: pluginVersion,
       deviceId: deviceId,
     );
+  }
+
+  static Future<String> getDeviceId(MethodChannel channel) async {
+    if (kIsWeb) {
+      return getWebDeviceId();
+    }
+    return await channel.invokeMethod('getDeviceId') ?? "";
+  }
+
+  /// Generates a unique device id for web and stores it in local storage
+  ///
+  /// If the device id is already stored, it returns the stored device id
+  ///
+  /// It acts as a pseudo-device ID for the duration of the interaction.
+  /// This ID will persist across sessions but not across browsers or devices,
+  /// and it can be cleared by the user (e.g., by clearing browser data).
+  static String getWebDeviceId() {
+    String? deviceId = html.window.localStorage['deviceId'];
+
+    if (deviceId == null) {
+      deviceId = Uuid().v4();
+      html.window.localStorage['deviceId'] = deviceId;
+    }
+
+    return deviceId;
   }
 
   const PlatformInfo._({
