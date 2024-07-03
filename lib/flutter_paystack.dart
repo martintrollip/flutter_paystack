c1e7b5c10fef9cf5c25fc129c2d765896e498d0f
Martin Trollip <13981100+martintrollip@users.noreply.github.com>
2024-07-03 10:29:18 +0200

    feat: Web - Expose typedef for functions

--- lib/flutter_paystack.dart
+++ lib/flutter_paystack.dart
@@ -5,3 +5,5 @@ export 'package:flutter_paystack/src/common/paystack.dart';
 export 'package:flutter_paystack/src/models/card.dart';
 export 'package:flutter_paystack/src/models/charge.dart';
 export 'package:flutter_paystack/src/models/checkout_response.dart';
+export 'package:flutter_paystack/src/platform/paystack_interface.dart'
+    show OnSuccess, OnLoad, OnError, OnCancel;
